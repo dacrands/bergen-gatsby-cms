90be816d145269aff395c633779d809488ad437d
da crands <davecrands@gmail.com>
2019-04-06 14:02:12 -0400

    Updating gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,46 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`]
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/img`,
+        name: 'uploads',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-relative-images`,
+            uploads: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              destionationDir: 'static',
+            },
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-netlify-cms`
+  ],
 }
