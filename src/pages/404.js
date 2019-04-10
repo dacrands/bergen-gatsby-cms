5fe20b3d64b9ed73e0cae9d021e4d082c869e589
da crands <davecrands@gmail.com>
2019-04-10 09:47:44 -0400

    Fixing document undefined during prod build in 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,23 @@
 import React from 'react'
+import { globalHistory } from "@reach/router"
 import Layout from '../components/Layout'
 
-export default () => (
-  <Layout>
-    <div className="container">
-      <header className="header">
-        <h1>
-          You Got Lost!
-        </h1>
-        <p>
-          There is no information at <code>{document.location.pathname.replace(/^\/([^\/]*).*$/, '$1')}</code>          
-        </p>
-        {/* <p>
-          To fix this error, please shut-down your computer for 75 minutes.
-        </p> */}
-      </header>
-    </div>
-  </Layout>
-)
+export default () => {    
+  return  (
+    <Layout>
+      <div className="container">
+        <header className="header">
+          <h1>
+            You Got Lost!
+          </h1>
+          <p>
+            There is no information at <code>{globalHistory.location.pathname}</code>          
+          </p>
+          {/* <p>
+            To fix this error, please shut-down your computer for 75 minutes.
+          </p> */}
+        </header>
+      </div>
+    </Layout>
+  )
+};
