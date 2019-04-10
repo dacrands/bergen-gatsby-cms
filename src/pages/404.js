4f0df371eca976f4489b23cb8454ec330b0c3f32
da crands <davecrands@gmail.com>
2019-04-09 22:50:05 -0400

    Adding semi-sarcastic 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default () => (
+  <Layout>
+    <div className="container">
+      <header className="header">
+        <h1>
+          You Got Lost!
+        </h1>
+        <p>
+          There is no information at <code>{document.location.pathname.replace(/^\/([^\/]*).*$/, '$1')}</code>          
+        </p>
+        <p>
+          To fix this error, please shut-down your computer for 75 minutes.
+        </p>
+      </header>
+    </div>
+  </Layout>
+)
