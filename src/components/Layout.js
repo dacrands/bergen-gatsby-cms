e87c72985300dd6e34dc3ad3324682014ba8b96c
da crands <davecrands@gmail.com>
2019-05-09 12:12:14 -0400

    Add react-helmet to Layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { Helmet } from "react-helmet"
+
 import Navbar from "./Navbar"
 import Footer from "./Footer"
 
@@ -6,6 +8,12 @@ import "../styles/main.scss"
 
 export default ({ children }) => (
   <div>
+    <Helmet>
+      <html lang="en" amp />
+      <meta charSet="utf-8" />
+      <meta name="description" content="Science, Technology, Engineering, Mathematics at Bergen Community College" />
+      <title>Bergen STEM</title>      
+    </Helmet>
     <Navbar />
     {children}
     <Footer />
