7ca48e679907446d67149c9fa731a7ad69d36494
da crands <davecrands@gmail.com>
2019-05-16 19:10:56 -0400

    Add success component

---
+++ src/pages/success.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+class Success extends React.Component {
+  render() {
+    if (!this.props.location.state) {
+      return (
+        <Layout>                
+          <div className="success">
+            <h1 className="success__check">&#10003;</h1>
+            <div className="success__text">
+              <h1>You didn't submit form.</h1>
+              <h3>But that's okay, enjoy this giant purple checkmark!</h3>
+            </div>
+            <Link className="button" to="/">
+                  Go back home
+            </Link>
+          </div>
+        </Layout>
+      )
+    }
+    return (
+      <Layout>                
+        <div className="success">
+          <h1 className="success__check">&#10003;</h1>
+          <div className="success__text">
+            <h1>Message Sent</h1>
+            <h3>Thank you!</h3>
+          </div>
+          <Link className="button" to="/">
+                Go back home
+          </Link>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Success
