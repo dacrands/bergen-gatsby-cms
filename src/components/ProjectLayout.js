14b23e64cc7f35eea76182af847a44263af4d82a
da crands <davecrands@gmail.com>
2019-07-03 14:09:43 -0400

    Add ProjectLayout

---
+++ src/components/ProjectLayout.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Navbar from "./Navbar"
+import ProjectAside from "./ProjectsAside"
+
+export default ({ children }) => (
+  <main>
+    <Navbar fixed={true} />
+    <ProjectAside />    
+    <div>
+      {children}
+    </div>
+  </main>
+)
